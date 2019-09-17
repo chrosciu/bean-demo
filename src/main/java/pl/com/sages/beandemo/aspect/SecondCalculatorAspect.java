121df045283364bd54e26ec505f7d668b9023545
Marcin Chrost <marcin.chrost@jcommerce.pl>
2019-09-17 17:01:27 +0200

    Enabled aspect once again

--- src/main/java/pl/com/sages/beandemo/aspect/SecondCalculatorAspect.java
+++ src/main/java/pl/com/sages/beandemo/aspect/SecondCalculatorAspect.java
@@ -16,7 +16,7 @@ public class SecondCalculatorAspect {
     @Around("execution(* pl.com.sages.beandemo.service.CalculatorService.*(..))")
     public Object aroundAnyCalculatorOperation(ProceedingJoinPoint joinPoint) throws Throwable {
         log.info("Before around calculator operation");
-        //Object rv = joinPoint.proceed();
+        Object rv = joinPoint.proceed();
         log.info("After around calculator operation");
         return Double.valueOf(-1);
     }
